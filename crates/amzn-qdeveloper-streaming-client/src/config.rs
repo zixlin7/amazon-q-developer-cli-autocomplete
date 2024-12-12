c5e88b5a5389a5e560ed18f62d6352efc3b4df71
Grant G <grant0417@users.noreply.github.com>
2024-12-12 14:56:00 -0800

    chore: fix docs tests with invalid codeblock (#243)

--- crates/amzn-qdeveloper-streaming-client/src/config.rs
+++ crates/amzn-qdeveloper-streaming-client/src/config.rs
@@ -1117,7 +1117,8 @@ impl Builder {
     /// # Examples
     ///
     /// Set the behavior major version to `latest`. This is equivalent to enabling the
-    /// `behavior-version-latest` cargo feature. ```no_run
+    /// `behavior-version-latest` cargo feature.
+    /// ```no_run
     /// use amzn_qdeveloper_streaming_client::config::BehaviorVersion;
     ///
     /// let config = amzn_qdeveloper_streaming_client::Config::builder()
@@ -1126,7 +1127,7 @@ impl Builder {
     ///     .build();
     /// let client = amzn_qdeveloper_streaming_client::Client::from_conf(config);
     /// ```
-    /// 
+    ///
     /// Customizing behavior major version:
     /// ```no_run
     /// use amzn_qdeveloper_streaming_client::config::BehaviorVersion;
@@ -1152,7 +1153,8 @@ impl Builder {
     /// # Examples
     ///
     /// Set the behavior major version to `latest`. This is equivalent to enabling the
-    /// `behavior-version-latest` cargo feature. ```no_run
+    /// `behavior-version-latest` cargo feature.
+    /// ```no_run
     /// use amzn_qdeveloper_streaming_client::config::BehaviorVersion;
     ///
     /// let config = amzn_qdeveloper_streaming_client::Config::builder()
@@ -1161,7 +1163,7 @@ impl Builder {
     ///     .build();
     /// let client = amzn_qdeveloper_streaming_client::Client::from_conf(config);
     /// ```
-    /// 
+    ///
     /// Customizing behavior major version:
     /// ```no_run
     /// use amzn_qdeveloper_streaming_client::config::BehaviorVersion;
